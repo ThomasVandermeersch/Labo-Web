2c5128e7795b60683a4dd87b5fa6a317d754dd39
ThomasVECAM <17030@ecam.be>
2021-04-10 20:16:02 +0200

    create router and navbar

--- Shop-frontend/src/app/app-routing.module.ts
+++ Shop-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
