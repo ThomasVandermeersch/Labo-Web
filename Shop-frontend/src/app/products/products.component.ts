87ed16d3e1433184c1d8b0d2db2bcd6d93f9ae6c
ThomasVandermeersch <17030@ecam.be>
2021-05-01 16:36:29 +0200

    add a filter

--- Shop-frontend/src/app/products/products.component.ts
+++ Shop-frontend/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Subscription } from 'rxjs';
+import { compareTwoStrings} from 'string-similarity';
 
 @Component({
   selector: 'app-products',
@@ -12,7 +13,11 @@ export class ProductsComponent implements OnInit {
 
   constructor(private productService : ProductService) {}
 
+
+
   ngOnInit() {
+
+
     if(!this.productService.isLoaded){
       // this.productSubscription = this.productService.productSubject.subscribe(
       //   (products: any[]) => {
@@ -32,11 +37,24 @@ export class ProductsComponent implements OnInit {
     }
 
   }
-}
 
-// this.service.getOrders().subscribe(
-//   (resp) => {
-//     console.log(resp);
-//     this.orders = resp//[{"id": 123, "totalPrice":23},{"id": 123, "totalPrice":23},{"id": 123, "totalPrice":29},{"id": 123, "totalPrice":23}]
-//   }
-// )
+  filter(value){
+
+    if(value==""){
+      this.products = this.productService.products
+    }
+    else{
+      value = value.toLowerCase()
+      const filterList = []
+      this.productService.products.forEach((product)=>{
+        console.log(product.name)
+        console.log(value)
+        console.log(compareTwoStrings(product.name,value))
+        if(  compareTwoStrings(product.name.toLowerCase(),value) >0.65){
+          filterList.push(product)
+        }
+      })
+      this.products = filterList
+    }
+  }
+}
